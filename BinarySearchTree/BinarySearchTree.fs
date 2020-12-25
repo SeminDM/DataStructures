5dfbf62d288d3faf70afb8ac8472f0f25bccff31
Dmitry Semin <amatoryslot@rambler.ru>
2020-12-25 21:13:44 +0300

    Add removing of node; add some tests

--- BinarySearchTree/BinarySearchTree.fs
+++ BinarySearchTree/BinarySearchTree.fs
@@ -4,6 +4,8 @@ type Tree = Node of int * Tree * Tree | Empty
 
 let leaf value = Node(value, Empty, Empty)
 
+let data = function Empty -> failwith "!" | Node(d,_,_) -> d
+
 let rec insert tree value =
     match tree with
     | Empty -> leaf value
@@ -15,12 +17,39 @@ let rec insert tree value =
         match r with
         | Empty -> Node(v, l, (leaf value))
         | r -> Node(v, l, (insert r value))
+
+let rec remove root value =
+    
+    let rec maxNode root =
+        match root with
+        | Empty
+        | Node(_,_,Empty) -> root
+        | Node(_,_,r) -> maxNode r
+
+    match root with
+    | Empty -> root
+    | Node(v,l,r) when value < v -> Node(v, (remove l value), r)
+    | Node(v,l,r) when value > v -> Node(v, l, (remove r value))
+    | Node(_,Empty,Empty) -> Empty
+    | Node(_,Empty,r) -> r
+    | Node(_,l,Empty) -> l
+    | Node(_,l,r) -> 
+        let maxValue = (maxNode>>data)l 
+        Node(maxValue, (remove l maxValue), r)
+
+let find root value = failwith ""
+
+let min root = failwith ""
+
+let max root = failwith ""
+
+let next root value = failwith ""
+
+let prev root value = failwith ""
     
 let left = function Empty | Node(_,Empty,_) -> None | Node(_,l,_) -> Some l
 
-let right = function Empty | Node(_,_,Empty)-> None | Node(_,_,r) -> Some r
-
-let data = function Empty -> failwith "!" | Node(d,_,_) -> d
+let right = function Empty | Node(_,_,Empty) -> None | Node(_,_,r) -> Some r
 
 let create items = Seq.fold insert Empty items
 
@@ -35,3 +64,14 @@ let preorder root =
 let postorder root =
     let rec walk = function Empty -> [] | Node(v,l,r) ->  walk l @ walk r @ [v]
     walk root
+
+let breadthFirst root =
+    let rec walk nodes result =
+        match nodes with
+        | [] -> result
+        | h::t ->
+            match h with
+            | Empty -> walk t result
+            | Node(v,Empty,Empty) -> walk t (v::result)
+            | Node(v,l,r) -> walk (t @ [l;r]) (v::result)
+    walk [root] [] |> List.rev
--- Test/Tests.fs
+++ Test/Tests.fs
@@ -5,24 +5,66 @@ open BinarySearchTree
 open FsUnit.Xunit
 open Xunit
 
+let initialList() = [31; 15; 24; 47; 36; 52; 7]
+
 [<Fact>]
 let ``Data is retained`` () =
-    let treeData = create [4]
-    treeData |> data |> should equal 4
-    treeData |> left |> should equal None
-    treeData |> right |> should equal None
+    let tree = create [4]
+    tree |> data |> should equal 4
+    tree |> left |> should equal None
+    tree |> right |> should equal None
 
 [<Fact>]
-let ``Can sort complex tree`` () =
-    let treeData = create [2; 1; 3; 6; 7; 5]
-    inorder treeData |> should equal [1; 2; 3; 5; 6; 7]
+let ``Inorder tree walk`` () =
+    let tree = create <| initialList()
+    inorder tree |> should equal [7; 15; 24; 31; 36; 47; 52]
 
 [<Fact>]
-let ``Can sort complex tree2`` () =
-    let treeData = create [2; 1; 3; 6; 7; 5]
-    preorder treeData |> should equal [1; 2; 3; 5; 6; 7]
+let ``Preorder tree walk`` () =
+    let tree = create <| initialList()
+    preorder tree |> should equal [31; 15; 7; 24; 47; 36; 52]
 
 [<Fact>]
-let ``Can sort complex tree3`` () =
-    let treeData = create [2; 1; 3; 6; 7; 5]
-    postorder treeData |> should equal [1; 2; 3; 5; 6; 7]
+let ``Postorder tree walk`` () =
+    let tree = create <| initialList()
+    postorder tree |> should equal [7; 24; 15; 36; 52; 47; 31]
+
+[<Fact>]
+let ``Breadth-first tree walk`` () =
+    let tree = create <| initialList()
+    breadthFirst tree |> should equal [31; 15; 47; 7; 24; 36; 52]
+
+[<Fact>]
+let ``Remove maximum`` () =
+    let tree = create <| initialList()
+    remove tree 52 |> inorder |> should equal [7; 15; 24; 31; 36; 47]
+
+[<Fact>]
+let ``Remove minimum`` () =
+    let tree = create <| initialList()
+    remove tree 7 |> inorder |> should equal [15; 24; 31; 36; 47; 52]
+
+[<Fact>]
+let ``Remove node which has both left and right children`` () =
+    let tree = create <| initialList()
+    remove tree 15 |> inorder |> should equal [7; 24; 31; 36; 47; 52]
+
+[<Fact>]
+let ``Remove root`` () =
+    let tree = create <| initialList()
+    remove tree 31 |> inorder |> should equal [7; 15; 24; 36; 47; 52]
+
+[<Fact>]
+let ``Remove node without left child`` () =
+    let tree = create [31; 15; 24; 47; 36; 52; 7; 28; 23; 20; 30]
+    remove tree 28 |> inorder |> should equal [7; 15; 20; 23; 24; 30; 31; 36; 47; 52]
+
+[<Fact>]
+let ``Remove node without right child`` () =
+    let tree = create [31; 15; 24; 47; 36; 52; 7; 28; 23; 20; 30]
+    remove tree 23 |> inorder |> should equal [7; 15; 20; 24; 28; 30; 31; 36; 47; 52]
+
+[<Fact>]
+let ``Remove node`` () =
+    let tree = create [31; 15; 24; 47; 36; 52; 7; 28; 23; 20; 30; 22; 21]
+    remove tree 24 |> inorder |> should equal [7; 15; 20; 21; 22; 23; 28; 30; 31; 36; 47; 52]
